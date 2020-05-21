f6b984494b41a036f5409e2e4312d203e50dee1c
kimhaneui <kimhaneui2@naver.com>
2020-05-21 12:44:36 +0900

    사용자 정보 화면 초안 작업

---
+++ app/src/main/java/com/example/andriodbasic02_0521/UserActivity.java
@@ -0,0 +1,26 @@
+package com.example.andriodbasic02_0521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
