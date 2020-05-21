b4a6d1c3a0c0031e0c0c2257b0e7b2c7573bbd9b
kimhaneui <kimhaneui2@naver.com>
2020-05-21 13:09:43 +0900

    사용자정보화면 => Base액티비티 상속 => 주석추가

--- app/src/main/java/com/example/andriodbasic02_0521/BaseActivity.java
+++ app/src/main/java/com/example/andriodbasic02_0521/BaseActivity.java
@@ -2,10 +2,17 @@ package com.example.andriodbasic02_0521;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public abstract class BaseActivity extends AppCompatActivity {
+//상속만을 위해 존재하는 액티비티 따라서 abstract(실제 액티비티가 아님)
 
+//    어느화면인지 알려주는 변수 만들고 this로대입
+    public Context mContext = this;
+
+
+//    자주구현하는 매소드=> 추상메소드로  정의
     public abstract void setupEvents();
     public abstract void setValues();
 }
--- app/src/main/java/com/example/andriodbasic02_0521/MainActivity.java
+++ app/src/main/java/com/example/andriodbasic02_0521/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.andriodbasic02_0521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
+import android.database.DatabaseUtils;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.andriodbasic02_0521.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
         setupEvents();
         setValues();
     }
 
     @Override
     public void setupEvents() {
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = binding.nameEdt.getText().toString();
+                Intent intent = new Intent(mContext, UserActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 
