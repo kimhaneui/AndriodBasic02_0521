e0e0f2d248d8448e9496d6f508b52a3370ef1741
kimhaneui <kimhaneui2@naver.com>
2020-05-21 12:36:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/andriodbasic02_0521/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.andriodbasic02_0521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
